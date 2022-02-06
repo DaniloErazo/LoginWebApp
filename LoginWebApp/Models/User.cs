c3dfc8d53cf6a7cacd987ab1f4652de0b095e704
DaniloErazo <erazodanilo1218@gmail.com>
2022-02-06 16:43:08 -0500

    Change born date format and input. Changed port for the project

--- LoginWebApp/Models/User.cs
+++ LoginWebApp/Models/User.cs
@@ -1,10 +1,14 @@
-﻿namespace LoginWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginWebApp.Models
 {
     public class User
     {
         public String ID { get; set; }
         public String Name { get; set; }
         public String LastName { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime BornDate { get; set; }
         public String Password { get; set; }
     }
