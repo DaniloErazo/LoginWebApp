63b592b3866341c5ca285967e3cfb36570b59a78
DaniloErazo <erazodanilo1218@gmail.com>
2022-02-06 14:02:17 -0500

    User class created with attributes

---
+++ LoginWebApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LoginWebApp.Models
+{
+    public class User
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public DateTime BornDate { get; set; }
+        public String Password { get; set; }
+    }
+}
